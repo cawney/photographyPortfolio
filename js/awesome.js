f7c844e490ed2d88be3ab0f89c04b5defd4273e6
James Carney <james.carneydev@gmail.com>
2015-10-16 14:12:06 -0400

    Added lightbox for portfolio
    Time to make the portfolio run, but as of now, it is kind of working

--- js/awesome.js
+++ js/awesome.js
@@ -35,19 +35,44 @@ jQuery(function(){
 });
 
 
+
+/**************************************
+Background Header Image
+**************************************/
+
+
+/*
+$(document).ready(function(){
+	console.log("Begin Function");
+	var n = $('.header-img').height();
+	console.log("The height is " + n);
+	console.log("End Function");
+});
+*/
+
+
 /**************************************
 Lazy Load
 **************************************/
 
 
 $(window).scroll(function() {
-  console.log('Lazy Load Running');
-   $("<p>").removeClass("some_class");
+  //console.log('Lazy Load Running');
+   $("p").removeClass("some_class");
    if($(window).scrollTop() + $(window).height() == $(document).height()) {
        //you are at bottom
-       console.log('You are at bottom');
+    //   console.log('You are at bottom');
        $("<p>").addClass("animated fadeInUp");
    }
+});
+
+
+window.scroll(function(){
+	console.log("begin function");
+	var h = $(document).scrollTop();
+	console.log(h);
+
+	console.log("end function");
 });
 
 
@@ -70,4 +95,49 @@ $(document).ready(function() {
     });
   });
 });
+
+
+
+/**************************************
+Lightbox
+**************************************/
+ 
+
+
+$('.portfolio-img').hover(function(){
+	$(this).toggleClass("tada"); //Makes imgage shake and draws attention to people to click on it.
+});
+
+$(".portfolio-img").click(function(){ //Makes lightbox appear
+
+	console.log("Begin Function");
+	$(".lightbox").toggleClass('hide'); //Reveals the lightbox //Originally set to hide.
+
+	//Get the image and display it dynamically
+	$('.portfolio-img').ready(function() {
+
+		//Get the image source
+		var imgSrc = $('.portfolio-img').attr('src');
+
+		//Add an image to the lightbox with correct source
+		$('.lightbox-img').append('<img src="' + imgSrc + '">'); //Adds img attribute with source to HTML
+
+	});
+	
+
+	//Button to leave the lightbox when needed.
+	$('button').click(function(){ //Makes the button hide the JS
+		$('.lightbox').addClass('hide');
+	});
+
+	console.log("End Function");
+});
+//Closes the lightbox
+	// $(".ligthbox>p").click(function(){
+	// 	alert("Testing");
+	// }); 
+
+
+
+
 
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
-###Photography Portfolio
+##Photography Portfolio
 
-####This is a readme for this photography portfolio
+###This is a readme for this photography portfolio
+
+#####Link to everything
+Here is a link for this [theme] (http://www.jamescarney.github.io/photographyPortfolio)
+
+#####Todo List
+*	Make a pretty lightbox for portfolio
